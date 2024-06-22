c00a9e25c5bfa2375bdcbe44e3f049649cdbf224
tadas.kinderis <tadas.kinderis@gmail.com>
2024-06-22 08:51:42 +0300

    config static page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
